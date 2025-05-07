cd93abe7240e1b6f74e25eec4a6d496be01adf83
zineflix <zineflix0@gmail.com>
2025-05-07 09:40:17 +0800

    Update anime-details.js

--- Anime/js/anime-details.js
+++ Anime/js/anime-details.js
@@ -73,11 +73,6 @@ window.tmdbId = tmdbId;
 
         <h2>Watch Anime</h2>
         
-        <label>
-         Season:
-         <select id="season-select"></select>
-        </label>
-        
         <div class="controls">
           <label>
             Episode:
@@ -109,30 +104,6 @@ window.tmdbId = tmdbId;
 
         <iframe id="stream-frame" src="" width="100%" height="500" allowfullscreen allow="autoplay; encrypted-media" sandbox="allow-scripts allow-same-origin"></iframe>
       `;
-
-fetch(`https://api.jikan.moe/v4/anime/${anime.mal_id}/relations`)
-  .then(res => res.json())
-  .then(relationData => {
-    const seasons = [anime]; // Include current anime as first season
-    relationData.data.forEach(rel => {
-      if (rel.relation.toLowerCase().includes("sequel") || rel.relation.toLowerCase().includes("prequel")) {
-        rel.entry.forEach(entry => seasons.push(entry));
-      }
-    });
-
-    const seasonSelect = document.getElementById("season-select");
-    seasons.forEach((s, index) => {
-      const option = document.createElement("option");
-      option.value = s.mal_id;
-      option.textContent = s.title;
-      seasonSelect.appendChild(option);
-    });
-
-    seasonSelect.addEventListener("change", () => {
-      const selectedId = seasonSelect.value;
-      window.location.search = `?id=${selectedId}`;
-    });
-  });
 
       
       // Streaming setup
