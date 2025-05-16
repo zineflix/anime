e07dacd5279b6281f08b6379472fd3ad0f09e4da
zineflix <zineflix0@gmail.com>
2025-05-16 22:37:07 +0800

    Create contact-us.js

---
+++ Anime/js/contact-us.js
@@ -0,0 +1,5 @@
+// Menu Bar Start //
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  document.getElementById('nav-links').classList.toggle('active');
+});
+// Menu Bar End //
